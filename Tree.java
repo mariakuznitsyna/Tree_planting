f859dc149f824a214d4b255b08549873b201e525
Maria Kuznitsyna <66224658+mariakuznitsyna@users.noreply.github.com>
2020-09-06 14:56:13 +0200

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+  public static void main(String[] args){
+    //We’re going to introduce ourselves and print a tree to the screen.
+    System.out.println("Hey there, I’m Maria and I’m learning to code in Java!");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println("  ****  ");
+    System.out.println(" ****** ");
+    System.out.println(" ****** ");
+    System.out.println("  ****  ");
+    System.out.println("   ||  ");   
+  }
+}
